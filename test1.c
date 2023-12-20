d1fe92d29e6cd9b202e5613930463fd457ce7418
robert.plsicik <robert.plsicik@OVY54>
2023-12-20 14:28:27 +0100

    Commit test1

--- test1.c
+++ test1.c
@@ -1,9 +1,33 @@
-#include <stdio.h>
 #include <time.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
 #include "refModel.h"
 
-int main(int argc, char** argv){
+int endStep = 10;
+uint8_t i_sg[3, endStep];
 
+int main(int argc, char** argv){
+    int step = 0;
+    
+    while(step <= endStep){
+        //uint8_t* evaluate_o_ctl(uint8_t i_nrst,uint8_t i_signal[3]);
+        uint8_t temp_i_sg[];
+        for(int i=0;i<3;i++){
+             temp_i_sg[i] = i_sg[i,step]
+        }
+        evaluate_o_ctl(1,temp_i_sg);
+        step++;
+    }
 
     return 0;
 }
+
+bool compare(uint8_t inputArr[]){
+    //Load reference data
+    //Take inputArr argument
+    //In cycle compare booth
+    
+
+}
+
